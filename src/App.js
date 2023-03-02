786ff1ca87d9c6d3e6b5cfb552c02e936a716ab9
Deepsavaliya2507 <deepsavaliya2507@gmail.com>
2023-03-02 09:13:24 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import Product from './Product';
 import Extra from './Extra';
 import Extrawork from './Extrawork';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
 
   return (
     <div>
-      <Product />
-      <Extra />
-      <Extrawork />
+      <Routes>
+        <Route path="/" element={<Product />} />
+        <Route path="/extra" element={<Extra />} />
+        <Route path="/extrawork" element={<Extrawork />} />
+      </Routes>
     </div>
 
   );
